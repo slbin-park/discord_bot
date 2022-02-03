72729c5bee4b022caf5f6374afd996004c2e64c0
slbin-park <smpts00@naver.com>
2022-02-03 17:28:52 +0900

    Add read/write json file

--- src/config/createdb/check_db.js
+++ src/config/createdb/check_db.js
@@ -2,9 +2,12 @@ const fs = require('fs');
 const mkdirp = require('mkdirp');
 const path = require('path');
 const rdf = fs.readFileSync(path.join('json', 'server', 'server_id.json'))
-const json_server = JSON.parse(rdf);
+let json_server = JSON.parse(rdf);
 
 const check_db = () => {
+    json_server.server.push(12345)
+    let data = JSON.stringify(json_server);
+    fs.writeFileSync(path.join('json', 'server', 'server_id.json'), data);
     console.log(json_server)
 }
 module.exports = {
