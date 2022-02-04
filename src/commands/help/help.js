89855e6417b6204c7ce460e6f5e23a8b78c6304a
slbin-park <smpts00@naver.com>
2022-02-04 19:11:23 +0900

    Add help command

--- src/commands/help/help.js
+++ src/commands/help/help.js
@@ -1,9 +1,14 @@
 
+
+const command = "```css\n!도움말 \n!체크 \n!디비생성 \n```"
+
+
 const help = (msg) => {
 
     const username = '<@' + msg.author.id + '>' // 멘션
     if (msg.content.startsWith('!도움말')) {
         msg.reply(username + '!```css\n  #b 안녕하세요\n파이어봇입니다.```');
+        msg.reply(command);
     }
 
     if (msg.content === '!내정보') {
